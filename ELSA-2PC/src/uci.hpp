8e6eddf9fac5a12851c3f63bfa5defcadc69de23
ariana <arianahejazyan@gmail.com>
2025-02-16 05:16:55 -0800

    feat(uci): add handleCommand and handleUCI

--- ELSA-2PC/src/uci.hpp
+++ ELSA-2PC/src/uci.hpp
@@ -1,8 +1,35 @@
 #ifndef UCI_HPP
 #define UCI_HPP
 
+#include <iostream>
+#include <sstream>
+#include "gamestate.hpp"
+
+using namespace std;
 namespace ELSA
 {
+
+void handleUCI()
+{
+    cout << "id name ELSA-2PC" << endl;
+    cout << "id author Ariana Hejazyan" << endl;
+    cout << "uciok" << endl;
+}
+
+void handleCommand(GameState& state, const string& command)
+{
+    stringstream ss(command);
+    string token;
+    ss >> token;
+
+    try {
+
+    if (command == "uci") handleUCI();
+
+    } catch (const exception& e) {
+        cout << e.what() << endl;
+    }
+}
 
 }; // namespace
 

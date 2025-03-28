00824713c123334b01216fc3aa76f5affc54ed0a
ariana <arianahejazyan@gmail.com>
2025-03-28 05:07:59 -0700

    feat(2pc): add entry point with CommandLine class

--- 2pc/main.cc
+++ 2pc/main.cc
@@ -1,28 +1,11 @@
-#ifndef MAIN_CC
-#define MAIN_CC
+#include "commandline.h"
 
-#include <iostream>
-#include <sstream>
-#include "gamestate.h"
-#include "uci.h"
-#include "utility.h"
-#include "zobrist.h"
-
-using namespace std;
-using namespace ELSA;
+using namespace talia;
 
 int main(int argc, char* argv[])
 {
-    GameState state;
-
-    initZobrist();
-
-    initGameState(state, startpos);
-    
-    string command;
-    while (getline(cin, command) && command != "quit") handleCommand(state, command);
-
+    CommandLine commandline;
+    commandline.run();
+ 
     return 0;
 }
-
-#endif

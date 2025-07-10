4c0d90c989edb2011c942cdd01b603898cb89a6b
ariana <arianahejazyan@gmail.com>
2025-07-10 16:17:30 +0330

    test(nnue): add ReLU tests

---
+++ athena/tests/nnue/activations/test_ReLU.cc
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include "nnue/activations/nnue_ReLU.h"
+
+TEST(TestReLU, Propagate)
+{
+    constexpr std::size_t size = 5;
+    const int input[] = {-3, 0, 5, -10, 7};
+    int output[size] = {};
+
+    athena::ReLU<size> ReLU;
+    ReLU.propagatge(input, output);
+
+    const int expected[] = {0, 0, 5, 0, 7};
+    for (int i = 0; i < size; ++i)
+    {
+        EXPECT_EQ(output[i], expected[i]);
+    }
+}

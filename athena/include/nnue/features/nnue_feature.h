e23ccd99b5ee07d38fcb6fabcf2633947bb1926e
ariana <arianahejazyan@gmail.com>
2025-07-10 10:28:57 +0330

    feat: add feature_index method

---
+++ athena/include/nnue/features/nnue_feature.h
@@ -0,0 +1,19 @@
+#ifndef NNUE_FEATURE_H
+#define NNUE_FEATURE_H
+
+#include <cstdint>
+#include "chess.h"
+
+namespace athena
+{
+
+    int feature_index(Square sq, PieceClass pc)
+    {
+        auto piece = pc.piece();
+        auto color = pc.color();
+        return color + (piece * 4) + (sq * 24);
+    }
+
+} // namespace athena
+
+#endif /* NNUE_FEATURE_H */

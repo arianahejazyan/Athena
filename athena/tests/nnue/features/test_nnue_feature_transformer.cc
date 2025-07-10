4920a59ca9981be828c42f29dc606c906dce4ed3
ariana <arianahejazyan@gmail.com>
2025-07-10 17:24:35 +0330

    test(nnue): add feature transformer tests

---
+++ athena/tests/nnue/features/test_nnue_feature_transformer.cc
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include "nnue/features/nnue_feature_transformer.h"
+#include "position.h"
+
+namespace athena
+{
+
+    class TestFeatureTransformer : public ::testing::Test
+    {
+    protected:
+        FeatureTransformer transformer;
+    };
+
+    TEST_F(TestFeatureTransformer, TransformEmptyPosition)
+    {
+        Position pos;
+        pos.board.clear();
+        pos.board.setSQ(C5, PieceClass(Queen, Blue));
+
+        int features[Lx0] = {};
+
+        transformer.transform(pos, features);
+
+        features[feature_index(C5, PieceClass(Queen, Blue))] = 1;
+    }
+
+} // namespace athena

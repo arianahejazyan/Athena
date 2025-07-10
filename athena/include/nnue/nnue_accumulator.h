f502cf8fe2e8de00dab1c9b8b2d176b71a02aad7
ariana <arianahejazyan@gmail.com>
2025-07-10 10:51:25 +0330

    feat: add accumulator classes

---
+++ athena/include/nnue/nnue_accumulator.h
@@ -0,0 +1,26 @@
+#ifndef NNUE_ACCUMULATOR_H
+#define NNUE_ACCUMULATOR_H
+
+#include <cstdint>
+#include "chess.h"
+
+namespace athena
+{
+
+    template <std::size_t size>
+    class Accumulator
+    {
+    private:
+        alignas(CacheLineSize) int data[size];
+    };
+
+    template <std::size_t size>
+    class AccumulatorStack
+    {
+    private:
+        std::vector<Accumulator<size>> stack;
+    };
+
+} // namespace athena
+
+#endif /* NNUE_ACCUMULATOR_H */

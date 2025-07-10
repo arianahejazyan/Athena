4655ca037fba474bf8b20d7158a3f9a235517ee6
ariana <arianahejazyan@gmail.com>
2025-07-10 11:03:09 +0330

    feat: add nnue class

---
+++ athena/include/nnue/nnue.h
@@ -0,0 +1,17 @@
+#ifndef NNUE_H
+#define NNUE_H
+
+#include "position.h"
+
+namespace athena
+{
+
+    class NNUE
+    {
+    public:
+        int32_t evaluate(const Position &pos);
+    };
+
+} // namespace athena
+
+#endif /* NNUE_H */

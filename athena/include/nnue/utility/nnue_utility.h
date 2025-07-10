b29e8b3cd8148605135ae7f966dea2cbcfe91cd9
ariana <arianahejazyan@gmail.com>
2025-07-10 10:35:57 +0330

    feat: add clipped

---
+++ athena/include/nnue/utility/nnue_utility.h
@@ -0,0 +1,15 @@
+#ifndef NNUE_UTILITY_H
+#define NNUE_UTILITY_H
+
+namespace athena
+{
+
+    template <typename T>
+    T clipped(T value, T min, T max)
+    {
+        return std::min(std::max(value, min), max);
+    }
+
+} // namespace athena
+
+#endif /* NNUE_UTILITY_H */

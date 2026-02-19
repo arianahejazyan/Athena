a6656ccac9e07b730839f70e5c9250427f14f8b9
ariana hejazyan <arianahejazyan@gmail.com>
2026-02-19 23:40:36 +0330

    refactor(uci): Remove UCI conversion functions and introduce UCIOption class for better organization

--- athena/src/uci.h
+++ athena/src/uci.h
@@ -7,17 +7,9 @@
 namespace athena
 {
 
-PieceClass UCI2PieceClass(std::string_view pieceClass) noexcept;
-Square UCI2Square(std::string_view square) noexcept;
-Piece UCI2Piece(char piece) noexcept;
-Color UCI2Color(char color) noexcept;
+class UCIOption
+{
 
-std::string PieceClass2UCI(PieceClass pieceClass) noexcept;
-std::string square2UCI(Square square) noexcept;
-std::string piece2UCI(Piece piece) noexcept;
-std::string color2UCI(Color color) noexcept;
-
-Move UCI2Move(std::string_view move) noexcept;
-std::string move2UCI(Move move) noexcept;
+};
 
 } // namespace athena

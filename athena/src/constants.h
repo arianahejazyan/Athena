7d808e04ac6e5cd1ef83450b506db9065bca66d7
ariana hejazyan <arianahejazyan@gmail.com>
2026-02-19 23:38:28 +0330

    feat(constants): Add constants

---
+++ athena/src/constants.h
@@ -0,0 +1,30 @@
+#pragma once
+
+namespace athena
+{
+
+constexpr std::size_t 
+GUIDES_SIZE    = 2,
+RANK_NB        = 16,
+FILE_NB        = 16,
+SQUARE_NB      = 256,
+PIECE_NB       = 6,
+COLOR_NB       = 4,
+PIECECLASS_NB  = 64,
+SIDE_NB        = 2,
+CASTLE_NB      = 8,
+MOVETYPE_NB    = 5,
+MOVEFLAG_NB    = 2,
+MOVE_NB        = 256,
+PLAY_NB        = 32,
+HOME_RANK      = 2,
+PROMOTION_RANK = 11,
+CRAWL_NB       = 8,
+SLIDE_NB       = 4,
+PUSH_NB        = 2,
+TAKE_NB        = 2,
+CHUNK_NB       = 4,
+CHUNK_SIZE     = 64,
+CACHELINE_SIZE = 64;
+
+} // namespace athena

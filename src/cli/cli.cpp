6468126b67084f8af740c10d90ffdf9f2c501051
ariana hejazyan <arianahejazyan@gmail.com>
2026-06-18 20:00:31 +0330

    feat: implement CLI

--- src/cli/cli.cpp
+++ src/cli/cli.cpp
@@ -1,277 +1,103 @@
-#include <iostream>
-#include "cli.h"
-#include "constants.h"
-#include "fen.h"
-#include "utility.h"
-#include "movegen.h"
-#include "uci.h"
-#include "perft.h"
+#include <sstream>
+#include "cli/cli.h"
 
-namespace athena
-{
+namespace athena::cli {
 
-// CommandLineInterface::CommandLineInterface()
-// {
-//     auto cmdUCI        = app.add_subcommand("uci"       )->callback([this] { handleUCI();        });
-//     auto cmdIsReady    = app.add_subcommand("isready"   )->callback([this] { handleIsReady();    });
-//     auto cmdSetOption  = app.add_subcommand("setoption" )->callback([this] { handleSetOption();  });
-//     auto cmdUCINewGame = app.add_subcommand("ucinewgame")->callback([this] { handleUCINewGame(); });
-//     auto cmdMakemove   = app.add_subcommand("makemove"  )->callback([this] { handleMakemove();   });
-//     auto cmdUndomove   = app.add_subcommand("undomove"  )->callback([this] { handleUndomove();   });
-//     auto cmdPos        = app.add_subcommand("pos"       )->callback([this] { handlePos();        });
-//     auto cmdFEN        = app.add_subcommand("fen"       )->callback([this] { handleFEN();        });
-//     auto cmdGo         = app.add_subcommand("go"        )->callback([this] { handleGo();         });
-//     auto cmdStop       = app.add_subcommand("stop"      )->callback([this] { handleStop();       });
-//     auto cmdQuit       = app.add_subcommand("quit"      )->callback([this] { handleQuit();       });
-//     auto cmdPerft      = app.add_subcommand("perft"     )->callback([this] { handlePerft();      });
-//     auto cmdPrint      = app.add_subcommand("print"     )->callback([this] { handlePrint();      });
+void CLI::run(int argc, char *argv[]) {
 
-//     ///
-//     cmdPos->add_option("type", pos_mode)->required()->check(CLI::IsMember({"startpos", "fen"}));
-//     cmdPos->allow_extras();
-
-//     ///
-//     cmdPrint->add_flag("-d,--debug", debug);
-
-//     // Init starting position
-//     pos.init(STARTPOS[setup = Setup::Modern]);
-// }
-
-CLI::CLI()
-{
-    pos_.init(STARTPOS);
-    // pos_.init("B-0,0,0,0-1,1,1,1-1,1,1,1-0,0,0,0-0-x,x,x,yR,yN,yB,yK,yQ,yB,yN,yR,x,x,x/x,x,x,yP,yP,yP,yP,yP,yP,yP,yP,x,x,x/x,x,x,8,x,x,x/bR,bP,10,gP,gR/bN,bP,10,gP,gN/bB,bP,10,gP,gB/bQ,bP,10,gP,gK/bK,bP,10,gP,gQ/bB,bP,10,gP,gB/bN,bP,10,gP,gN/bR,bP,3,rP,6,gP,gR/x,x,x,8,x,x,x/x,x,x,rP,rP,1,rP,rP,rP,rP,rP,x,x,x/x,x,x,rR,rN,rB,rQ,rK,rB,rN,rR,x,x,x");
-
-    // auto setup = GameSetup::Modern;
-
-    // Move moves[MOVE_NB];
-    // std::size_t n = 0;
-    // n += generate_noisy_moves(pos_, moves + n, GameSetup::Modern);
-    // n += generate_quiet_moves(pos_, moves + n, GameSetup::Modern);
-    // std::cout <<"n: "<< n<<std::endl;
-
-    // for (std::size_t i = 0; i < n; ++i)
-    // {
-    //     // std::cout <<moves[i].uci(true)<<std::endl;
-    //     if (moves[i].uci(true) == "g3g4") {
-    //         pos_.makemove(moves[i], setup);
-    //         break;
-    //     }
-    // }
-
-    // pos_.print();
-    // std::cout << pos_.inCheck(Color(1)) << std::endl;
-            
-    // pos_.bitboard(Color(0)).print();
-    // pos_.bitboard(Color(1)).print();
-    // pos_.bitboard(Color(2)).print();
-    // pos_.bitboard(Color(3)).print();
-
-    // pos_.bitboard(Piece(0)).print();
-    // pos_.bitboard(Piece(1)).print();
-    // pos_.bitboard(Piece(2)).print();
-    // pos_.bitboard(Piece(3)).print();
-    // pos_.bitboard(Piece(4)).print();
-    // pos_.bitboard(Piece(5)).print();
-    // uint64_t nodes = 0;
-    // for (std::size_t i = 0; i < n; ++i)
-    // {
-    //     const auto move = moves[i];
-    //     pos_.makemove(move, setup);
-        
-    //     if (pos_.inCheck(pos_.turn().prev()))
-    //     {
-    //         pos_.undomove(move, setup);
-    //         continue;
-    //     }
-
-    //     nodes += perft(pos_, depth - 1, setup);
-    //     pos_.undomove(move, setup);
-    // }
-
-}
-
-void CLI::launch()
-{
-    while (std::getline(std::cin, line_)) 
+    std::string line;
+    while (std::getline(std::cin, line))
     {
-        tokens_ = tokenize(std::string_view(line_), ' ');
-        execute();
-    }
-}
-
-void CLI::execute()
-{
-    try
-    {
-        if (tokens_.empty())
-        {
-            std::cout << "info string empty command" << std::endl;
-            return;
-        }
-
-        auto name = tokens_.front();
-
-        if (name == "uci"       ) handleUCI();        else 
-        if (name == "isready"   ) handleIsReady();    else 
-        if (name == "setoption" ) handleSetOption();  else 
-        if (name == "ucinewgame") handleUCINewGame(); else 
-        if (name == "pos"       ) handlePos();        else 
-        if (name == "go"        ) handleGo();         else 
-        if (name == "stop"      ) handleStop();       else 
-        if (name == "quit"      ) handleQuit();       else 
-        if (name == "perft"     ) handlePerft();      else 
-        if (name == "print"     ) handlePrint();      else
-        if (name == "fen"       ) handleFen();        else
-        if (name == "movegen"   ) handleMoveGen();    else
-        {
-            std::cout << "info string unknown command '" << name << "'" << std::endl;
-            return;
+        if (line.empty()) continue;
+        std::istringstream is(line);
+        std::string name;
+        is >> name;
+        if (name.empty()) continue;
+        if (name == "uci"       ) uci();         else 
+        if (name == "isready"   ) isready();     else 
+        if (name == "setoption" ) setoption(is); else 
+        if (name == "ucinewgame") ucinewGame();  else 
+        if (name == "pos"       ) pos(is);       else 
+        if (name == "go"        ) go(is);        else 
+        if (name == "stop"      ) stop();        else 
+        if (name == "quit"      ) quit();        else {
+            std::cout << "info string" << "unknown command: '" << line << std::endl;
         }
     }
-
-    catch (const std::exception& e) {
-        std::cout << "info string " << e.what() << std::endl;
-    }
 }
 
-// void CommandLineInt erface::launch()
-// {
-//     std::string line;
-//     while (std::getline(std::cin, line)) if (!line.empty()) execute(line);
-// }
-
-// void CommandLineInterface::execute(std::string& line)
-// {
-//     try 
-//     {
-//         char* argv[64];
-//         int argc = tokenize(line, argv, 64);
-
-//         // Reset configurations to default values
-//         debug = false;
-
-//         // Parse and execute command
-//         app.clear();
-//         app.parse(argc, argv);
-//     }
-
-//     catch (const CLI::ParseError& e) { std::cout << "info error " << e.what() << std::endl; }
-//     catch (const std::exception & e) { std::cout << "info error " << e.what() << std::endl; }
-// }
-
-void CLI::handleUCI()
-{
-    std::cout 
-    << "id name Athena" << '\n'
-    << "id author Ariana Hejazyan" << '\n' 
-    << "uciok" << std::endl;
+void CLI::uci() {
+    std::cout << "id name Athena\n";
+    std::cout << "id author Ariana Hejazyan\n";
+    std::cout << "option name Setup type combo default modern var modern var classic\n";
+    std::cout << "uciok" << std::endl;
 }
 
-void CLI::handleIsReady()
-{
+void CLI::isready() {
     std::cout << "readyok" << std::endl;
 }
 
-void CLI::handleSetOption()
-{
-}
+void CLI::setoption(std::istream& args) {
 
-void CLI::handleUCINewGame()
-{
-}
+    std::string token, name, value;
 
-void CLI::handlePos()
-{   
-    std::size_t index = 1;
-    FEN fen;
-
-    if (tokens_[index] == "fen")
-    {
-        index++;
-        fen = tokens_[index];
+    for (int i=0; i<4; i++) {
+        if (!(args >> token)) {
+            std::cout << "info string expected format: setoption name <name> value <value>\n";
+            return;
+        }
+        if (i == 1) name  = token;
+        if (i == 3) value = token;
     }
 
-    else fen = STARTPOS;
-
-    pos_.init(fen);
-    // index++;
-    // index++;
-
-    // while (index < tokens_.size())
-    // {
-    //     pos_.makemove(Move(tokens_[index], pos_));
-    //     index++;
-    // }
+    engine_.setOption(name, value);
 }
 
-void CLI::handleGo()
-{
+void CLI::ucinewGame() {
 }
 
-void CLI::handleStop()
-{
+void CLI::pos(std::istream& args) {
+
+    std::string token;
+    args >> token;
+
+    std::string fen;
+    if (token == "startpos") {
+        if (args >> token) {
+            fen = token == "modern" ? 
+            chess::Position::startpos(chess::Castle::Setup::Modern ):
+            chess::Position::startpos(chess::Castle::Setup::Classic);
+            args >> token;
+        } else {
+            std::cout << "info string expected modern or classic after 'startpos' command\n";
+        }
+    } else if (token == "fen") {
+        if (args >> token) {
+            fen = token;
+            args >> token;
+        } else {
+            std::cout << "info string expected FEN string after 'fen' command\n";
+        }
+    } else {
+        std::cout << "info string position expected startpos or fen\n";
+        return;
+    }
+
+    engine_.setPosition(fen);
+
+    if (token == "moves") {
+        while (args >> token) engine_.applyMove(token);
+    }
 }
 
-void CLI::handleQuit()
-{
-    std::exit(0);
+void CLI::go(std::istream& args) {
 }
 
-void CLI::handlePerft()
-{
-    std::size_t index = 1;
-    Perft perft;
-
-    int depth = std::stoi(std::string(tokens_[index]));
-    // index++;
-
-    // bool split   = false;
-    // bool tabular = false;
-    // if (tokens_.size() > index)
-    // {
-    //     if (tokens_[index] == "-s" || tokens_[index] == "--split")
-    //     {
-    //         split = true;
-    //     }
-
-    //     if (tokens_[index] == "-t" || tokens_[index] == "--tabular")
-    //     {
-    //         tabular = true;
-    //     }
-    // }
-
-    std::cout << "running perft at depth " << depth << std::endl;
-
-    Perft::Options options;
-    options.depth = depth;
-    options.split = false;
-
-    perft.run(pos_, options, GameSetup::Modern);
-    
-    // std::cout << "perft completed" << std::endl;
+void CLI::stop() {
 }
 
-void CLI::handlePrint()
-{
-    pos_.print();
-}
-
-void CLI::handleFen()
-{
-    std::cout << pos_.fen() << std::endl;
-}
-
-void CLI::handleMoveGen()
-{
-
-    // Move moves[MOVE_NB];
-    // std::size_t num = genAllPseudoMoves(pos_, moves);
-    // for (std::size_t i = 0; i < num; i++)
-    // {
-    //     std::cout << move2UCI(moves[i]) << std::endl;
-    // }
-    // std::cout << "total pseudo legal moves: " << num << std::endl;
+void CLI::quit() {
+    std::exit(EXIT_SUCCESS);
 }
 
 } // namespace athena
--- src/cli/cli.h
+++ src/cli/cli.h
@@ -1,57 +1,26 @@
 #pragma once
 
-// #include <CLI/CLI.hpp>
-#include <vector>
-#include <string>
-#include "chess.h"
-#include "position.h"
+#include <iostream>
+#include "core/engine/engine.h"
 
-// #include "perft.h"
+namespace athena::cli {
 
-namespace athena
-{
+class CLI {
+public:
+    CLI() = default;
+    void run(int argc, char *argv[]);
 
-class CLI
-{
-    public:
-    CLI();
+private:
+    void uci();
+    void isready();
+    void setoption(std::istream& args);
+    void ucinewGame();
+    void pos(std::istream& args);
+    void go(std::istream& args);
+    void stop();
+    void quit();
 
-    void launch();
-    void execute();
-
-    private:
-    std::string line_;
-    std::vector<std::string_view> tokens_;
-
-    // UCI commands
-    void handleUCI();
-    void handleIsReady();
-    void handleSetOption();
-    void handleUCINewGame();
-    void handlePos();
-    void handleGo();
-    void handleStop();
-    void handleQuit();
-
-    // Other commands
-    void handlePerft();
-    void handlePrint();
-    void handleFen();
-    void handleMoveGen();
-
-    private:
-    Position pos_;
-
-    /// @brief CLI11 application instance for command-line parsing
-    // CLI::App app{"Athena Engine CLI"};
-
-    // /// Configurations
-    // Position pos;
-    // Perft perft;
-    // bool debug;
-    // Setup setup;
-    // std::string pos_mode;
-    // std::vector<std::string> moves;
+    core::Engine engine_;
 };
 
 } // namespace athena

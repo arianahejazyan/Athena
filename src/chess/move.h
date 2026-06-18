1c10b1d8aad7e11607bc3922e029965fcc8ae0b5
ariana hejazyan <arianahejazyan@gmail.com>
2026-06-18 20:01:22 +0330

    feat: implement move

--- src/chess/move.h
+++ src/chess/move.h
@@ -5,96 +5,57 @@
 #include "piece.h"
 #include "castle.h"
 
-namespace athena
-{
+namespace athena::chess {
 
-// Forward declaration
-class Position;
+class Move {
+public: 
+    enum class Policy : uint8_t {
+        Normal = 0,
+        Stride = 1,
+        Evolve = 2,
+        Enpass = 3,
+        Castle = 4,
+    };
 
-// WARNING: Evolve can simultaneously be Enpass too
-enum class MoveType : uint8_t
-{
-    Crawl  = 0,
-    Slide  = 1,
-    Push   = 2,
-    Take   = 3,
-    Stride = 4,
-    Evolve = 5,
-    Enpass = 6,
-    Castle = 7,
-};
+    enum class Flag : uint8_t {
+        Noisy = 0,
+        Quiet = 1,
+        Legal = 2,
+    };
 
-enum class MoveFlag : uint8_t
-{
-    Noisy = 0,
-    Quiet = 1,
-};
-
-class Move
-{
-    public:
-
-    Move(Square source, Square target, MoveFlag flag, MoveType type) noexcept
-        : encoded_(static_cast<uint8_t>(source) | (static_cast<uint8_t>(target) << 24) | (static_cast<uint32_t>(type) << 8)) {}
-
-    Move(Square source, Square target, Side side) noexcept
-        : encoded_(static_cast<uint8_t>(source) | (static_cast<uint8_t>(target) << 24) | (static_cast<uint32_t>(MoveType::Castle) << 8) | (static_cast<uint8_t>(side) << 18)) {}
-
-    Move(Square source, Square target, Piece piece) noexcept
-        : encoded_(static_cast<uint8_t>(source) | (static_cast<uint8_t>(target) << 24) | (static_cast<uint32_t>(MoveType::Evolve) << 8) | (static_cast<uint8_t>(piece) << 12)) {}
-
-    Move(Square source, Square target, Color enpass) noexcept
-        : encoded_(static_cast<uint8_t>(source) | (static_cast<uint8_t>(target) << 24) | (static_cast<uint32_t>(MoveType::Enpass) << 8) | (static_cast<uint8_t>(enpass) << 15)) {}
-
-    Move(Move move, bool pseudo) noexcept
-        : encoded_(move.encoded_ | (pseudo << 20)) {}
-
-    Move() noexcept = default;
-
-    Square source() const noexcept { 
-        return Square(encoded_ & 0xFF); 
+    constexpr Move() noexcept = default;
+    constexpr Move(
+        Square::ID   source, // 8 bits
+        Square::ID   target, // 8 bits
+        Piece::ID    evolve, // 3 bits
+        Color::ID    enpass, // 3 bits
+        Castle::Side castle, // 2 bits
+        Move::Policy policy  // 3 bits
+            ) noexcept : move_(
+            (static_cast<uint32_t>(source) << 0 ) |
+            (static_cast<uint32_t>(target) << 24) |
+            (static_cast<uint32_t>(evolve) << 8 ) |
+            (static_cast<uint32_t>(enpass) << 11) |
+            (static_cast<uint32_t>(castle) << 14) |
+            (static_cast<uint32_t>(policy) << 16) ) {}
+ 
+    auto source() const noexcept { return static_cast<Square::ID  >((move_ & 255)        ); }
+    auto target() const noexcept { return static_cast<Square::ID  >((move_ >> 24)        ); }
+    auto evolve() const noexcept { return static_cast<Piece::ID   >((move_ >> 8 ) & 0b111); }
+    auto enpass() const noexcept { return static_cast<Color::ID   >((move_ >> 11) & 0b111); }
+    auto castle() const noexcept { return static_cast<Castle::Side>((move_ >> 14) & 0b001); }
+    auto policy() const noexcept { return static_cast<Move::Policy>((move_ >> 16) & 0b111); } 
+    
+    std::string uci(bool board16x16 = false) const noexcept {
+        std::string str = "";
+        str += Square(source()).uci(board16x16);
+        str += Square(target()).uci(board16x16);
+        str += evolve() == Piece::ID::Empty ? "" : std::to_string(Piece(evolve()).uci());
+        return str;
     }
 
-    Square target() const noexcept { 
-        return Square(encoded_ >> 24); 
-    }
-
-    Side castle() const noexcept { 
-        return Side((encoded_ >> 18) & 0x1);
-    }
-
-    Color enpass() const noexcept { 
-        return Color((encoded_ >> 15) & 0x7); 
-    }
-
-    Piece evolve() const noexcept { 
-        return Piece((encoded_ >> 12) & 0x7);
-    }
-
-    MoveType type() const noexcept { 
-        return MoveType((encoded_ >> 8) & 0x7); 
-    }
-
-    MoveFlag flag() const noexcept { 
-        return MoveFlag((encoded_ >> 11) & 0x1); 
-    }
-
-    bool pseudo() const noexcept {
-        return encoded_ & (1 << 20);
-    }
-
-    void setpseudo(Square sq) noexcept {
-        encoded_ |= ((source() == sq) << 20);
-    }
-
-    // UCI Conversion
-    std::string uci(bool board16x16 = false) const noexcept;
-
-    // Conversion operator
-    explicit constexpr operator uint32_t() const noexcept { return encoded_; }
-
-    private:
-    uint32_t encoded_;
+private:
+    uint32_t move_;
 };
 
 } // namespace athena

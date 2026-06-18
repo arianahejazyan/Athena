8ab850aefae7b8268e6fc1f6522653b2dea02ec5
ariana hejazyan <arianahejazyan@gmail.com>
2026-06-18 19:59:11 +0330

    tests: add perft gtests

--- tests/perft_test.cc
+++ tests/perft_test.cc
@@ -1,29 +1,36 @@
 #include <gtest/gtest.h>
-#include "constants.h"
-#include "fen.h"
+#include <cstdint>
+#include "chess/castle.h"
+#include "chess/position.h"
 #include "perft.h"
-#include "position.h"
 
 using namespace athena;
 
+#define MODERN  chess::Castle::Setup::Modern
+#define CLASSIC chess::Castle::Setup::Classic
+struct Test {
+    int depth;
+    uint64_t nodes;
+    std::string fen;
+    chess::Castle::Setup setup;
+};
+
+inline const std::array<Test, 7> tests_modern = {{
+    {1, 20        , chess::Position::startpos(MODERN), MODERN},
+    {2, 395       , chess::Position::startpos(MODERN), MODERN},
+    {3, 7800      , chess::Position::startpos(MODERN), MODERN},
+    {4, 152050    , chess::Position::startpos(MODERN), MODERN},
+    {5, 3452310   , chess::Position::startpos(MODERN), MODERN},
+    {6, 77430383  , chess::Position::startpos(MODERN), MODERN},
+    {7, 1735784286, chess::Position::startpos(MODERN), MODERN},
+}};
+
 TEST(PerftTest, Modern) {
-    Position pos;
-    pos.init(STARTPOS);
-
-    Perft perft;
-    Perft::Options opt{};
-
-    EXPECT_EQ(perft.perft(pos, 1, GameSetup::Modern), 20ULL);
-    EXPECT_EQ(perft.perft(pos, 2, GameSetup::Modern), 395ULL);
-    EXPECT_EQ(perft.perft(pos, 3, GameSetup::Modern), 7800ULL);
-    EXPECT_EQ(perft.perft(pos, 4, GameSetup::Modern), 152050ULL);
+    const auto modern = chess::Castle::Setup::Modern ;
+    chess::Position pos;
+    for (auto test: tests_modern) {
+        pos.set_setup(test.setup);
+        pos.init(test.fen);
+        EXPECT_EQ(tests::run_perft_tests(pos, test.depth, false), test.nodes);
+    }
 }
-
-// From Terminator
-// ply=1, positions=20
-// ply=2, positions=395
-// ply=3, positions=7800
-// ply=4, positions=152050
-// ply=5, positions=3452310
-// ply=6, positions=77430383
-// ply=7, positions=1735784286

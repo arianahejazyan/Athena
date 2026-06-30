f47f62eb0c528804662b278fe0a8b376c9c4f64c
ariana hejazyan <arianahejazyan@gmail.com>
2026-06-30 22:46:20 +0330

    feat: add perft function for performance testing in chess engine

---
+++ src/chess/perft.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <cstdint>
+#include "chess/position.h"
+#include "chess/movegen.h"
+
+namespace athena::chess {
+
+inline uint64_t perft(chess::Position& pos, int depth) {
+
+    if (depth == 0) return 1;
+
+    chess::Move moves[chess::MOVE_NB];
+    int num_moves = generate_legal_moves(pos, moves);
+
+    if (depth == 1) return num_moves;
+
+    uint64_t nodes = 0;
+    for (int i = 0; i < num_moves; i++) {
+        const auto& move = moves[i];
+        pos.make_move(move);
+        nodes += perft(pos, depth - 1);
+        pos.undo_move(move);
+    }
+
+    return nodes;
+}
+
+} // namespace athena

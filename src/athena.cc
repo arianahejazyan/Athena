c263c902c0df962973bb5de345f1eeffffce3b9e
ariana hejazyan <arianahejazyan@gmail.com>
2026-07-01 14:03:57 +0330

    refactor: simplify main function by integrating CLI execution and removing legacy command handling

--- src/athena.cc
+++ src/athena.cc
@@ -1,38 +1,8 @@
-#include <iostream>
 #include "cli/cli.h"
 
 using namespace athena;
 
-void version() {
-    std::cout << "v0.0.0" << std::endl;
-}
-
-void help() {
-    std::cout << "UCI commands:\n"
-              << "  uci           - identify engine\n"
-              << "  isready       - ping engine\n"
-              << "  setoption     - set engine option\n"
-              << "  ucinewgame    - reset for new game\n"
-              << "  position      - set board position\n"
-              << "  go            - start searching\n"
-              << "  stop          - stop searching\n"
-              << "  quit          - exit\n";
-}
-
-int main(int argc, char *argv[])
-{
-    if (argc > 1)
-    {
-        std::string name = argv[1];
-        if (name == "--version") { version(); return EXIT_SUCCESS; } else
-        if (name == "--help"   ) { help();    return EXIT_SUCCESS; } else {
-            std::cerr << "info string unknown command line argument: " << name << std::endl;
-            return EXIT_FAILURE;
-        }  
-    }
-
+int main(int argc, char* argv[]) {
     cli::CLI cli;
-    cli.run(argc, argv);
-
-    return EXIT_SUCCESS;
+    return cli.run(argc, argv);
 }

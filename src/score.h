46668daed5f3110e7c5e4eb3c71400bbf6141635
ariana hejazyan <arianahejazyan@gmail.com>
2026-07-04 19:45:28 +0330

    feat: add score type

---
+++ src/score.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "chess/constants.h"
+#include <cstdint>
+
+namespace athena {
+
+using Score = int32_t;
+
+constexpr Score SCORE_INFINITE = 999999;
+constexpr Score SCORE_MATE     = 100000;
+constexpr Score SCORE_DRAW     = 0;
+
+inline Score abs(Score score) noexcept {
+    return score >= 0 ? score : -score;
+}
+
+inline bool isMateScore(Score score) noexcept {
+    return SCORE_MATE - chess::PLAY_NB <= abs(score);
+}
+
+inline int mateIn(Score score) noexcept {
+    return SCORE_MATE - abs(score);
+}
+
+} // namespace athena::core
